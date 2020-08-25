a7f4e9ef965f192acb689dcd348daa06c2fbc75a
J Tait Loughridge <lochridge08@gmail.com>
2020-08-25 11:48:31 -0500

    added a dropdown

--- js/index.js
+++ js/index.js
@@ -2,10 +2,9 @@
 const myButton = document.getElementById('refreshQuote');
 const chuckSays = document.getElementById('chuckSays');
 const submitFormButton = document.getElementById('submitForm')
-const defaultCategory = "dev"
+let category = "dev"
 
-// function getQuote(category) {}
-const getQuote = (category) => {
+const getQuote = () => {
     const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
 
     get(url).then(function(fetchResponse) {
@@ -13,18 +12,43 @@ const getQuote = (category) => {
     })
 }
 
+const getCategories = () => {
+    const url = `https://api.chucknorris.io/jokes/categories`;
+    const dropDownMenu = document.getElementById("categoryInput");
+
+    get(url).then(function(categoryArray) {
+        categoryArray
+            .filter((category) => category !== "explicit")
+            .map(function (category){
+                const categoryOption = document.createElement('option');
+                categoryOption.value = category;
+                categoryOption.text = category;
+                dropDownMenu.append(categoryOption);
+        });
+    })
+}
+
 myButton.addEventListener('click', function (e) {
     event.preventDefault(e);
-    getQuote(defaultCategory);
+    getQuote();
 });
 
-submitFormButton.addEventListener('click', function (e) {
+// submitFormButton.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const userInput = document.getElementById('categoryInput');
+//     const category = userInput.value;
+//     getQuote(category);
+// });
+
+const getChuckQuotes =document.getElementById("getChuckQuotes");
+getChuckQuotes.addEventListener("submit", e => {
     e.preventDefault();
     const userInput = document.getElementById('categoryInput');
-    const category = userInput.value;
-    getQuote(category);
+    category = userInput.value;
+    getQuote();
 });
 
 (function () {
-getQuote(defaultCategory)
+    getCategories();
+    getQuote();
 })();
